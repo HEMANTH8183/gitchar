5ac96302fe8dd37eb4cf4560558219d1603153ff
HEMANTH8183 <hemanth8183.xworkz@gmail.com>
2021-12-21 08:08:37 +0530

    char file

---
+++ hemanthchar.java
@@ -0,0 +1,12 @@
+class Hemanth {
+	public static void main (String []args){
+	char []friends = {'!','@','#','$','%','&',};
+	System.out.println(friends[0]);	
+	System.out.println(friends[1]);
+	System.out.println(friends[2]);
+	System.out.println(friends[3]);
+	System.out.println(friends[4]);
+	System.out.println(friends[5]);
+	}
+}
+	
